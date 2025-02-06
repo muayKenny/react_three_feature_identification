439dc143e1d0d0303151ac55030c2afed9b99b71
Kenny Sheftel <kennysheftel@gmail.com>
2025-02-06 15:15:13 -0800

    init

---
+++ app/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Model } from './model/model';
+
+const App = () => (
+  <React.Fragment>
+    <Model />
+  </React.Fragment>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
